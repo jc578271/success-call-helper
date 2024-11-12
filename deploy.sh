7be1af30d9badb3c890c119f4ca6df47b7904b63
jc578271 <lehoang.nguyen@my.jcu.edu.au>
2024-11-12 16:58:23 +0700

    ok

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "platform: $1";
 rm docs/$1/CallHelper.apk
-mv app/release/app-release.apk docs/$1/CallHelper.apk
+cp app/release/app-release.apk docs/$1/CallHelper.apk
 cd docs && git add . && git commit -m "update CallHelper.apk" && git push -u origin master
